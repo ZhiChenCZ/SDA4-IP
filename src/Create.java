da35a3b5e64add58e7f80e97942f2481f09825d0
ZhiChenCZ <cz.chenzhi92@gmail.com>
2018-09-19 15:59:19 +0200

    Testing

---
+++ src/Create.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+public class Create {
+
+    private String title;
+    private String dueDate;
+
+    public Create(String title, String dueDate){
+
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    public
+}
