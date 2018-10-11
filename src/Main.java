9260bcc1a02c94be0e6e0b6b53147c407aad9f7a
ZhiChenCZ <cz.chenzhi92@gmail.com>
2018-10-11 21:34:41 +0200

    Main, to start using toDoly

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+
+        TaskManager tm = new TaskManager();
+        View me = new View(tm);
+        me.toDoly();
+
+
+    }
+}
